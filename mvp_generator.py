6873241a9e2f4e194c999b3756363e974103b356
Andrew Gaytan <andrewutmail@gmail.com>
2024-03-19 07:30:27 -0500

    completed team total and player share of PAR metrics

--- mvp_generator.py
+++ mvp_generator.py
@@ -18,7 +18,34 @@ player_game_data_df = pd.read_csv("NBA Data/Player Game Data.csv")
 mvp_ranking_df = player_game_data_df.copy()
 
 # calculate player share of PAR (points, assist, rebounds) for the season
+
+# calculate team totals of PAR
+mvp_ranking_df["team_PTS"] = mvp_ranking_df.groupby(
+    ["TEAM_ID", "GAME_ID"])["PTS"].transform("sum")
+
+mvp_ranking_df["team_AST"] = mvp_ranking_df.groupby(
+    ["TEAM_ID", "GAME_ID"])["AST"].transform("sum")
+
+mvp_ranking_df["team_REB"] = mvp_ranking_df.groupby(
+    ["TEAM_ID", "GAME_ID"])["REB"].transform("sum")
+
 # calculate share of PAR each player is responsible for
+mvp_ranking_df["share_of_team_PTS"] = (
+    mvp_ranking_df["PTS"] / mvp_ranking_df["team_PTS"])
+
+mvp_ranking_df["share_of_team_AST"] = (
+    mvp_ranking_df["AST"] / mvp_ranking_df["team_AST"])
+
+mvp_ranking_df["share_of_team_REB"] = (
+    mvp_ranking_df["REB"] / mvp_ranking_df["team_REB"])
+
+######################## START OF DEBUGGING SPACE #####################################
+pd.set_option('display.max_columns', None)
+print(mvp_ranking_df.head(20))
+########################## END OF DEBUGGING SPACE #####################################
+
+
+
 # caculate avg PAR for the season
 # calculate total PAR for the season
 
